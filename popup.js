d9f50a3c81ed53f438881515a783944a845b34b0
ilya <imakovoz93@gmail.com>
2018-08-25 01:37:19 -0400

    refactor to recursive to avoid api quota

--- popup.js
+++ popup.js
@@ -7,38 +7,56 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // var account = "124154600";
     // var tracker = "UA-124154600-4";
     var account = document.querySelector("#account").value;
+    var tracker = document.querySelector("#tracker").value;
+    var json = document.querySelector("#json").value;
+
     if (account === "") {
       account = "124154600";
     }
-    var tracker = document.querySelector("#tracker").value;
     if (tracker === "") {
       tracker = "UA-124154600-7";
     }
-    var json = document.querySelector("#json").value;
     if (json === "") {
       json = {
-        "1": "demandbase_sid",
-        "2": "company_name",
-        "3": "industry",
-        "4": "sub_industry",
-        "5": "employee_range",
-        "6": "revenue_range",
-        "7": "audience",
-        "8": "audience_segment",
-        "9": "marketing_alias",
-        "10": "city",
-        "11": "state",
-        "12": "country_name",
-        "13": "watch_list_account_type",
-        "14": "watch_list_account_status",
-        "15": "watch_list_campaign_code",
-        "16": "watch_list_account_owner"
+        "1": "Ydemandbase_sid",
+        "2": "Ycompany_name",
+        "3": "Yindustry",
+        "4": "Ysub_industry",
+        "5": "Yemployee_range",
+        "6": "Yrevenue_range",
+        "7": "Yaudience",
+        "8": "Yaudience_segment",
+        "9": "Ymarketing_alias",
+        "10": "Ycity",
+        "11": "Ystate",
+        "12": "Ycountry_name",
+        "13": "Ywatch_list_account_type",
+        "14": "Ywatch_list_account_status",
+        "15": "Ywatch_list_campaign_code",
+        "16": "Ywatch_list_account_owner"
       };
+      // json = {
+      //   "1": "demandbase_sid",
+      //   "2": "company_name",
+      //   "3": "industry",
+      //   "4": "sub_industry",
+      //   "5": "employee_range",
+      //   "6": "revenue_range",
+      //   "7": "audience",
+      //   "8": "audience_segment",
+      //   "9": "marketing_alias",
+      //   "10": "city",
+      //   "11": "state",
+      //   "12": "country_name",
+      //   "13": "watch_list_account_type",
+      //   "14": "watch_list_account_status",
+      //   "15": "watch_list_campaign_code",
+      //   "16": "watch_list_account_owner",
+      // };
     } else {
       json = JSON.parse(json);
     }
     var keys = Object.keys(json);
-
     // sanitize inputs
     keys.forEach(key => {
       json[key] = json[key]
@@ -80,9 +98,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
             token
         );
         // call back function once authenticated
-        x.onload = function() {
+        x.onload = () => {
           let list = {};
           var indexes = [];
+
           JSON.parse(x.response)["items"].forEach(item => {
             list[item["index"]] = item["name"];
             indexes.push(item["index"]);
@@ -106,36 +125,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
           }
 
           keys = keys.map(z => parseInt(z));
-          // testing api only
-          // for (var i = 0; i < keys.length; i++) {
-          for (var i = 0; i < 1; i++) {
-            if (indexes.filter(j => j == keys[i]).length > 0) {
+          var cbFunc = () => {
+            if (indexes.filter(j => j == keys[0]).length > 0) {
               // if updating
               var k = new XMLHttpRequest();
-              console.log(account);
-              console.log(tracker);
               k.open(
                 "PATCH",
-                `https://www.googleapis.com/analytics/v3/management/accounts/124154600/webproperties/UA-124154600-4/customDimensions/ga:dimension1?alt=json&access_token=${token}`
+                `https://www.googleapis.com/analytics/v3/management/accounts/${account}/webproperties/${tracker}/customDimensions/ga:dimension${
+                  keys[0]
+                }?alt=json&access_token=${token}`
               );
-              var body = {
-                name: "ilya504",
-                index: 1,
-                scope: "SESSION",
-                active: true
-              };
-              console.log(body);
-              // var body = {
-              //   name: json[keys[i]],
-              //   index: keys[i],
-              //   scope: "SESSION",
-              //   active: true
-              // };
-              k.setRequestHeader(
-                "Content-Type",
-                "application/json;charset=UTF-8"
-              );
-              k.send(JSON.stringify(body));
             } else {
               // if inserting
               var k = new XMLHttpRequest();
@@ -148,18 +147,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
                   "/customDimensions?alt=json&access_token=" +
                   token
               );
-              var body = {
-                name: json[keys[i]],
-                kind: "analytics#customDimension",
-                index: keys[i],
-                scope: "SESSION",
-                active: true,
-                accountId: account,
-                webPropertyId: tracker
-              };
-              k.send(JSON.stringify(body));
             }
-          }
+            var body = {
+              name: json[keys[0]],
+              index: keys[0],
+              scope: "SESSION",
+              active: true
+            };
+            k.setRequestHeader(
+              "Content-Type",
+              "application/json;charset=UTF-8"
+            );
+
+            k.onload = () => {
+              if (keys.length > 1) {
+                keys = keys.slice(1);
+                wait(2000);
+                cbFunc();
+              } else {
+                alert("Done");
+              }
+            };
+            k.send(JSON.stringify(body));
+          };
+          cbFunc();
         };
         // send request
         x.send();
@@ -183,3 +194,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
     chrome.identity.launchWebAuthFlow(options, function(redirectUri) {});
   };
 });
+function wait(ms) {
+  var start = new Date().getTime();
+  var end = start;
+  while (end < start + ms) {
+    end = new Date().getTime();
+  }
+}
